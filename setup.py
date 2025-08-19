fa99f9f27e5f2f7722207fa2853eaeb32e6d9d27
sharhan-alhassan <sharhanalhassan@gmail.com>
2025-08-19 15:37:01 +0000

    Fix build issues: correct requirements parsing in setup.py, update MANIFEST.in, and improve GitHub Actions workflow

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read()
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith('#')]
 
 setup(
     name='momopy',
@@ -16,7 +16,7 @@ setup(
     author_email='sharhanalhassan@gmail.com',
     url='https://github.com/sharhan-alhassan/momopy',
     packages=find_packages(exclude=['tests']),
-    install_requires=[requirements],
+    install_requires=requirements,
     keywords='momo mobile-money python package library',
     classifiers=[
         'Programming Language :: Python :: 3',
